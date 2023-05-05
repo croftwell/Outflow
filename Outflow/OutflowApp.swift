2573b3f1eb29c4df70cf6457757e1ac3bbdf7a0c
Ali Çavuşlu <cavuslumehmet@icloud.com>
2023-05-05 17:23:17 +0300

    Initial Commit

---
+++ Outflow/OutflowApp.swift
@@ -0,0 +1,17 @@
+//
+//  OutflowApp.swift
+//  Outflow
+//
+//  Created by Ali on 5.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct OutflowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
